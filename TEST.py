295e4a32343907dfc8fc9edaa0abd50342963186
MathildePilgaard <78751896+MathildePilgaard@users.noreply.github.com>
2021-12-16 16:14:09 +0100

    kode

--- TEST.py
+++ TEST.py
@@ -7,5 +7,14 @@ import  glob #package used to list files in Python
 import  sys
 import  traceback
 
+#Kode der henter og læser alle filer under mappen "J:\VOA" og samtlige undermapper:
 #File_object = open(r"path","Access_Mode")
 
+path =r'J:\VOA'
+list_files = []
+for root, dirs, files in os.walk(path):
+	for file in files:
+		list_files.append(os.path.join(root,file))
+for name in list_files:
+    print(name) 
+    
