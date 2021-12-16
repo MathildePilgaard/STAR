d70fd1fe80f6a2fcf7d2c024d9569c352553f3ee
MathildePilgaard <78751896+MathildePilgaard@users.noreply.github.com>
2021-12-16 15:53:22 +0100

    Pakker

--- TEST.py
+++ TEST.py
@@ -2,9 +2,10 @@
 message = 'hello world'
 print(message)
 
-import  glob
-import  os
+import  os #Package used to read file names in folder
+import  glob #package used to list files in Python
 import  sys
 import  traceback
 
+#File_object = open(r"path","Access_Mode")
 
