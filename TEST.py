a82500aa525b8be27612c28801722f2e29c7dea1
Mathilde Gade Pilgaard <B152366@PROD.SITAD.DK>
2021-12-23 11:58:18 +0100

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -7,18 +7,12 @@ import  sys
 import  traceback
 
 #Kode der henter og læser alle filer under mappen "J:\VOA" og samtlige undermapper:
-#File_object = open(r"path","Access_Mode")
-
-path =r'C:\J:\VOA'
-#Check om path er correct:
-os.path.isdir(path)
-
+#File_object = open(r"path","Access_Mode") Ved ikke om jeg skal bruge denne i stedet...?
+#path =r'C:\Users\Administrator.SHAREPOINTSKY\Desktop\Work'
+path = r'J:\VOA'
 list_files = []
 for root, dirs, files in os.walk(path):
 	for file in files:
 		list_files.append(os.path.join(root,file))
 for name in list_files:
     print(name)
-    
-    
-    
