7c44de93761e25e61296b574f5e19a38f6b9fe01
MathildePilgaard <78751896+MathildePilgaard@users.noreply.github.com>
2022-01-05 10:29:42 +0100

    edfefe

--- TEST.py
+++ TEST.py
@@ -1,18 +1,30 @@
-message = 'hello world'
-print(message)
-
 import  os #Package used to read file names in folder
 import  glob #Package used to list files in Python
 import  sys
 import  traceback
+from pathlib import Path
+
+message = 'hello Mathilde you got this'
+print(message)
 
 #Kode der henter og læser alle filer under mappen "J:\VOA" og samtlige undermapper:
 #File_object = open(r"path","Access_Mode") Ved ikke om jeg skal bruge denne i stedet...?
-#path =r'C:\Users\Administrator.SHAREPOINTSKY\Desktop\Work'
-path = r'J:\VOA'
+#Alternativ med python3: data_folder = Path("source_data/text_files/")
+
+path = r'J:\\VOA'
+
+#Tjek om path eksiterer:
+out = os.path.isfile("J:\\VOA")
+
+print(out)
+
 list_files = []
 for root, dirs, files in os.walk(path):
 	for file in files:
 		list_files.append(os.path.join(root,file))
 for name in list_files:
     print(name)
+
+
+
+    
