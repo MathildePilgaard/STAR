a7dcdb64f8a6f31430681170ce58b2ec6d373b90
MathildePilgaard <78751896+MathildePilgaard@users.noreply.github.com>
2022-02-01 13:20:17 +0100

    PDF-kladde

--- TEST.py
+++ TEST.py
@@ -6,25 +6,21 @@ from pathlib import Path
 import regex
 import PyPDF2
 import openpyxl
-import docx2txt
 import pdfplumber
+import re
 
 # 1) Kode der henter og læser alle filer under mappen "J:\VOA" og samtlige undermapper. 
 # Hvis med mellemrum da tilføj r foran path:
 # path = 'J:\VOA'
 
-#Til test:
+#Beyttes til test:
 path = r'J:\VOA\4Studenterbulen'
 
 #Tjek om VOA-path eksiterer:
 #out = os.path.isfile('J:\VOA\exception_print_opholdsgrundlag_mangler_mapning.xlsx')
 #print(out)
 
-# 2) Loop der printer alle filer i mappe og undermapper:
- 
-os.chdir(path)
-
-#3) Læs filer af TXT og .xls og printer sti:
+#2) Læs filer af TXT og .xls og printer sti:
 def read_files(file_path):
     with open(file_path, 'r') as file:
         file_contents = file.read()
@@ -54,6 +50,54 @@ for name in list_files:
         pass
     
 #4) Læs PDF'er med cpr-nummer:
+#Creating a pdf file object 
+pdfpath = PyPDF2.PdfFileReader(path)
 
-# creating a pdf file object 
-pdfFileObj = open('example.pdf', 'rb')
+def read_files(pdffile_pdfpath):
+    with open(pdffile_pdfpath, 'r') as pdffile:
+        pdffile_contents = pdffile.read()
+        return pdffile_contents
+
+def check_for_cpr_numbers(pdffile_contents):
+    match = regex.search("([0-9]{6}-[0-9]{4}|^[0-9]{9,10}$)", file_contents, regex.IGNORECASE)
+    #Læser nu ikke komma og punktum samt læser kun 9-10 antal hele tal.
+    return True if match else False
+
+list_pdffiles = []
+for root, dirs, files in os.walk(pdfpath):
+    print(f'Currently at {root}')
+    for file in pdffiles:
+        # print(file)
+        if file.endswith('pdf'):
+            list_files.append(os.pdfpath.join(root,file))
+
+print('Listing found files containing CPR:')
+for name in list_files:
+    file_contents = read_files(name)
+    if check_for_cpr_numbers(pdffile_contents) is True:
+        #Er true og false med her? Jeg har tilføjet 'is True'.
+        #Tilføj til ny liste der indeholder oversigt over alle filenavn der har cpr numre i sig
+        #returne listen af filnavne eller printe den, eller gemme i en ny fil
+        print(name)
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
